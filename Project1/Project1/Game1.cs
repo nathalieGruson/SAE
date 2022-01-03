adfea0394d7db83b7fe415c5f7bb06e26abbf77d
IUTAnnecy <benoit.diard@univ-smb.fr>
2022-01-03 16:16:54 +0100

    modif en noir

--- Project1/Project1/Game1.cs
+++ Project1/Project1/Game1.cs
@@ -42,7 +42,7 @@ namespace Project1
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Red);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
